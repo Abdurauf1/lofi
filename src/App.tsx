12b9fa5688bd39609cb68cc5c12503d543ba2479
Abdurauf <aabdurauf004@gmail.com>
2025-02-24 01:20:03 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Player } from "./components"
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-cover bg-center bg-[url(./assets/images/img1.jpg)]">
+      <Player />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Player.tsx
@@ -0,0 +1,14 @@
+import { FaPlay, FaBackward, FaForward, FaStop } from "react-icons/fa";
+const Player = () => {
+  return (
+    <div className="absolute bottom-10 left-10 backdrop-blur-xs bg-white/10 p-5">
+      <div className="flex items-center gap-3">
+        <FaBackward className="text-white cursor-pointer" />
+        <FaPlay className="text-white cursor-pointer" />
+        <FaForward className="text-white cursor-pointer" />
+      </div>
+    </div>
+  )
+}
+
+export default Player
